1b798f10aa8310648b79ed8b19e92a2ae840cd92
Shubham Shrivastava <shubham.s@neoflo.ai>
2026-06-09 18:21:22 +0530

    Collapsed sidebar shows clean 'N' mark; rename nav section to 'Cash Application - B2B'

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -52,7 +52,7 @@
     nav.innerHTML = `
       <div class="navgroup" id="navgroup-cashapp">
         <button class="navlink navlink--group is-open" id="cashapp-toggle">
-          ${icon("cashapp")}<span>Cash Application</span>
+          ${icon("cashapp")}<span>Cash Application - B2B</span>
           <svg class="navlink__chev" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 9l6 6 6-6"/></svg>
         </button>
         <div class="navsubs" id="cashapp-subs">${subs}</div>
